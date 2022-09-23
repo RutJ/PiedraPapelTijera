9a6be4ac5903875378449a86aefbb7cde609fac6
RutJ <jaquelinacruz.95@gmail.com>
2022-09-23 12:03:57 -0300

    Estructura

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,23 @@
-export default function Game() {
+import "./estilos/Home.css";
+import {BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
+import Home from './components/Home';
+import Juego from './components/Juego';
+import Desarrolladores from './components/Desarrolladores';
+
+
+function Game() {
   return (
-    <h1>Piedra Papel Tijera</h1>
-    
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/juego' element={<Juego/>}/>
+        <Route path='/desarrolladores' element={<Desarrolladores/>}/>
+      </Routes>
+    </Router>
+    <footer>Pie de Pagina</footer>
+    </>
   );
 }
 
-//export default App;
+export default Game;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import "../estilos/Home.css";
+import { Link } from "react-router-dom";
+import Inicio from "../img/principal.jpeg";
+export default function Home(){
+    return(
+        <div >
+            <h1>Piedra Papel o Tijera</h1>
+            <img src={Inicio} alt="Juego"/>
+
+            <Link to='/juego' className="btn-principal">Empezar a Jugar</Link>
+
+            <Link to='/desarrolladores' className="btn-principal">Conocenos</Link>
+        </div>
+        
+    )
+};
