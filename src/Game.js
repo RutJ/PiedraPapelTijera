c62ad8a7b471ea562efa845c4fb66a63aa4b4417
RutJ <jaquelinacruz.95@gmail.com>
2022-09-25 21:40:45 -0300

    Funcionando...

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,24 @@
-import "./estilos/Home.css";
+import "./Game.css";
 import {BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
 import Home from './components/Home';
 import Juego from './components/Juego';
 import Desarrolladores from './components/Desarrolladores';
+import Nav from 'react-bootstrap/Nav';
 
 
 function Game() {
   return (
-    <>
+    <div className='game'>
     <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/juego' element={<Juego/>}/>
-        <Route path='/desarrolladores' element={<Desarrolladores/>}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/juego' element={<Juego/>}/>
+      <Route path='/desarrolladores' element={<Desarrolladores/>}/>
+    </Routes>
+  </Router>
     <footer>Pie de Pagina</footer>
-    </>
+    </div>
+    
   );
 }
 
